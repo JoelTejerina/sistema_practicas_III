eb1af5d7c4d1e1eb73f464f0fa468bf152294fb8
TomasOneProge <tomasborstner@gmail.com>
2023-09-06 18:55:35 -0300

    Abriendo un coso

--- CapaVistaUsuario/Administrador/frmAltaProducto.Designer.cs
+++ CapaVistaUsuario/Administrador/frmAltaProducto.Designer.cs
@@ -51,6 +51,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Agregar Producto";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // groupBox1
             // 
@@ -143,6 +144,7 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "Guardar";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -152,6 +154,7 @@
             this.button3.TabIndex = 3;
             this.button3.Text = "Cerrar";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // frmAltaProducto
             // 
--- CapaVistaUsuario/Administrador/frmAltaProducto.cs
+++ CapaVistaUsuario/Administrador/frmAltaProducto.cs
@@ -16,5 +16,20 @@ namespace CapaVistaUsuario.Administrador
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
     }
 }
